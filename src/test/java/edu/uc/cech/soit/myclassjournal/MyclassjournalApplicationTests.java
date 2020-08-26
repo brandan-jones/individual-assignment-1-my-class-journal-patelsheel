c24ba5409127b0cebd3bf9afa092d52953fc4224
jonesbr <brandan.jones@uc.edu>
2020-08-25 21:07:59 -0400

    Initial commit

---
+++ src/test/java/edu/uc/cech/soit/myclassjournal/MyclassjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uc.cech.soit.myclassjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclassjournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
